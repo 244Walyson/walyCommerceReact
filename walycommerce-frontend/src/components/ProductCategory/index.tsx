069638135eb204242f035bd1e2f5064b8579958f
walyson <walysonmoises1342@gmail.com>
2023-09-24 00:40:21 -0300

    statics componets

---
+++ walycommerce-frontend/src/components/ProductCategory/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css'
+const ProductCategory = () => {
+  return (
+    <div className="dsc-category-container">
+    <div className="dsc-category">
+        Eletrônicos
+    </div>
+</div>
+  )
+}
+
+export default ProductCategory
