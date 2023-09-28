a483bb194780930fccf6910ccdad39e43669c570
walyson <walysonmoises1342@gmail.com>
2023-09-27 23:39:14 -0300

    categories dinamic

--- walycommerce-frontend/src/components/ProductDetailsCard/index.tsx
+++ walycommerce-frontend/src/components/ProductDetailsCard/index.tsx
@@ -3,6 +3,21 @@ import ProductCategory from '../ProductCategory'
 import './styles.css'
 
 const ProductDetailsCard = () => {
+
+  const categories = [
+    {
+      id: 1,
+      name: "Computadores"
+    },
+    {
+      id: 2,
+      name: "Eletrônicos"
+    },
+    {
+      id: 3,
+      name: "Importados"
+    }
+  ]
   return (
         <div className="dsc-card dsc-mb20">
           <div className="dsc-product-details-top dsc-line-bottom">
@@ -21,8 +36,11 @@ const ProductDetailsCard = () => {
               sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <div className="categories">
-            <ProductCategory name='Eletrônicos'></ProductCategory>
-            <ProductCategory name="Computadores"></ProductCategory>
+              {
+                categories.map(item =>(
+                  <ProductCategory key={item.id} name={item.name}></ProductCategory>
+                ))
+              }
             </div>
           </div>
         </div>
