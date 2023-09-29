8112ab4a61a0934b0156a469702947243d5537ee
walyson <walysonmoises1342@gmail.com>
2023-09-29 20:51:33 -0300

    links, header

--- walycommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ walycommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -5,6 +5,7 @@ import ProductDetailsCard from '../../../components/ProductDetailsCard'
 import './styles.css'
 import { findById } from '../../../Data/data'
 import { ProductDTO } from '../../../models/ProductModel'
+import { Link } from 'react-router-dom'
 
 const ProductDetails = () => {
 
@@ -16,7 +17,7 @@ const ProductDetails = () => {
         {product && <ProductDetailsCard product={product}></ProductDetailsCard>}
         <div className="dsc-btn-page-container">
             <ButtonPrimary text="Compar"></ButtonPrimary>
-            <ButtonInverse text="Início"></ButtonInverse>
+            <Link to={"/"}><ButtonInverse text="Início"></ButtonInverse></Link>
         </div>
       </section>
     </main>
