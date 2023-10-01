66a0332a00a807134f9b7ee4e4a2ccc36eb80802
walyson <walysonmoises1342@gmail.com>
2023-10-01 00:22:55 -0300

    get product by id with axios

--- walycommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ walycommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,21 +3,30 @@ import ButtonInverse from '../../../components/ButtonInverse'
 import ButtonPrimary from '../../../components/ButtonPrimary'
 import ProductDetailsCard from '../../../components/ProductDetailsCard'
 import './styles.css'
-import { findById } from '../../../Data/data'
 import { ProductDTO } from '../../../models/ProductModel'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ProductDetails = () => {
 
   const params = useParams()
-  const product: ProductDTO | undefined = findById(Number(params.productId))
+  const [product, seProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products/" + Number(params.productId))
+      .then(response => {
+        seProduct(response.data)
+      })    
+  }, [])
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product}></ProductDetailsCard>}
         <div className="dsc-btn-page-container">
-            <ButtonPrimary text="Compar"></ButtonPrimary>
-            <Link to={"/"}><ButtonInverse text="Início"></ButtonInverse></Link>
+          <ButtonPrimary text="Compar"></ButtonPrimary>
+          <Link to={"/"}><ButtonInverse text="Início"></ButtonInverse></Link>
         </div>
       </section>
     </main>
