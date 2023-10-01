18a3ef5cb7b39114514de16eee13addd9ca72fa3
walyson <walysonmoises1342@gmail.com>
2023-10-01 00:36:58 -0300

    findAll products, axios

--- walycommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ walycommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -3,13 +3,22 @@ import SearchBar from '../../../components/SearchBar'
 import CatalogCard from '../../../components/CatalogCard'
 import ButtonNextPage from '../../../components/ButtonNextPage'
 import { ProductDTO } from '../../../models/ProductModel'
-import { findAll } from '../../../Data/data'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Catalog = () => {
 
-  const product: ProductDTO[] = findAll()
+  const [product, setProducts] = useState<ProductDTO[]>()
+  const [page, setPage] = useState(0)
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products?page="+ page)
+    .then(response =>{
+      setProducts(response.data.content)
+    })
+  }, [])
 
   return (
     <main>
@@ -17,11 +26,11 @@ const Catalog = () => {
         <SearchBar></SearchBar>
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            {product && product.map((item)=>(
-              <Link to={`/product-details/${item.id}`}><CatalogCard product={item} key={item.id}></CatalogCard></Link>
-            ))}
+          {product && product.map((item) => (
+            <Link to={`/product-details/${item.id}`}><CatalogCard product={item} key={item.id}></CatalogCard></Link>
+          ))}
         </div>
-        <ButtonNextPage></ButtonNextPage>
+        <div onClick={() => setPage(page + 1)}><ButtonNextPage></ButtonNextPage></div>
       </section>
     </main>
   )
