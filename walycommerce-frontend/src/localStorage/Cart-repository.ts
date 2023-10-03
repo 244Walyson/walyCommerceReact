6156957d5e3e838432cbc9be95c52e5f9be70619
walyson <walysonmoises1342@gmail.com>
2023-10-03 13:55:11 -0300

    cart

--- walycommerce-frontend/src/localStorage/Cart-repository.ts
+++ walycommerce-frontend/src/localStorage/Cart-repository.ts
@@ -1,4 +1,4 @@
-import { OrderDTO } from "../models/Order";
+import { OrderDTO, OrderItemDTO } from "../models/Order";
 import { CART_KEY } from "../utils/system";
 
 
@@ -9,5 +9,14 @@ export function save(cart: OrderDTO){
 
 export function get(): OrderDTO{
     const str = localStorage.getItem(CART_KEY) || '{"items":[]}'
-    return JSON.parse(str)
+    const obj = JSON.parse(str)
+    const cart = new OrderDTO()
+    obj.items.forEach((x: OrderItemDTO) => {
+        cart.items.push(new OrderItemDTO(x.productId, x.quantity, x.name, x.price, x.imgUrl))
+    });
+    return cart
+}
+
+export function clear(){
+    localStorage.setItem(CART_KEY, '"items": []')
 }
--- walycommerce-frontend/src/routes/ClientHome/Cart/index.tsx
+++ walycommerce-frontend/src/routes/ClientHome/Cart/index.tsx
@@ -1,45 +1,46 @@
-import { OrderDTO, OrderItemDTO } from '../../../models/Order'
+import { OrderDTO } from '../../../models/Order'
 import * as cartService from '../../../services/cartService'
 import './styles.css'
-import { useEffect, useState } from 'react'
-
-const item1 : OrderItemDTO = new OrderItemDTO(
-    4, 1, 'pc gamer', 1200, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
-)
-const item2 : OrderItemDTO = new OrderItemDTO(
-    5, 2, "Rails for damies", 100.99, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
-)
+import { useState } from 'react'
 
 
 const Cart = () => {
-    const [cart, setCart] = useState<OrderDTO | undefined>(cartService.getCart())
-
+    const [cart, setCart] = useState<OrderDTO>(cartService.getCart())
+    console.log(cart)
     return (
         <main>
             <section id="cart-container-section" className="dsc-container">
-                <div className="dsc-card dsc-mb20">
-                    {cart && cart.items.map(item => (
-                        <div className="dsc-cart-item-container dsc-line-bottom" key={item.productId}>
-                            <div className="dsc-cart-item-left">
-                                <img src={item.imgUrl} alt={item.name} />
-                                <div className="dsc-cart-item-description">
-                                    <h3>{item.name}</h3>
-                                    <div className="dsc-cart-item-quantity-container">
-                                        <div className="dsc-cart-item-quantity-btn">-</div>
-                                        <p>{item.quantity}</p>
-                                        <div className="dsc-cart-item-quantity-btn">+</div>
+                {
+                    cart?.items.length == 0 ?
+                        <div>
+                            <h2 className='dsc-section-title dsc-mb20'>Seu carrinho está vazio</h2>
+                        </div>
+                        :
+                        <>
+                            <div className="dsc-card dsc-mb20">
+                                {cart.items.map(item => (
+                                    <div className="dsc-cart-item-container dsc-line-bottom" key={item.productId}>
+                                        <div className="dsc-cart-item-left">
+                                            <img src={item.imgUrl} alt={item.name} />
+                                            <div className="dsc-cart-item-description">
+                                                <h3>{item.name}</h3>
+                                                <div className="dsc-cart-item-quantity-container">
+                                                    <div className="dsc-cart-item-quantity-btn">-</div>
+                                                    <p>{item.quantity}</p>
+                                                    <div className="dsc-cart-item-quantity-btn">+</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="dsc-cart-item-right">
+                                            R$ {item.subTotal.toFixed(2)}                            </div>
                                     </div>
+                                ))}
+                                <div className="dsc-cart-total-container">
+                                    <h3>R$ {cart.total.toFixed(2)}</h3>
                                 </div>
                             </div>
-                            <div className="dsc-cart-item-right">
-                                R$ {item.price * item.quantity}
-                            </div>
-                        </div>
-                    ))}
-                    <div className="dsc-cart-total-container">
-                        <h3>R$ 15000,00</h3>
-                    </div>
-                </div>
+                        </>
+                }
                 <div className="dsc-btn-page-container">
                     <div className="dsc-btn dsc-btn-blue">
                         Finalizar pedido
