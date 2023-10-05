8d1b601b1445555e5ae71199c1fd78792a662f37
walyson <walysonmoises1342@gmail.com>
2023-10-05 01:36:43 -0300

    login form, login request

--- walycommerce-frontend/src/routes/Login/index.tsx
+++ walycommerce-frontend/src/routes/Login/index.tsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
 import './styles.css'
+import { CredentialsDTO } from '../../models/auth'
+import * as authService from '../../services/auth-service'
 
 const Login = () => {
+
+    const [formData, setFormData] = useState<CredentialsDTO>({username: "", password: ""})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        authService.loginRequest(formData).then(response => {
+            console.log(response.data)
+        }).catch(error => {
+            console.log("erro login", error)
+        })
+    }
+    const handleInputChange = (e) => {
+        const value = e.target.value
+        const name = e.target.name
+        setFormData({...formData, [name]: value})
+    }
+
   return (
     <main>
       <section id="login-section" className="dsc-container">
@@ -9,16 +29,16 @@ const Login = () => {
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
-                <input className="dsc-form-control dsc-input-error" type="text" placeholder="Email" />
+                <input onChange={handleInputChange} name='username' value={formData.username} className="dsc-form-control dsc-input-error" type="text" placeholder="Email" />
                 <div className="dsc-form-error">Campo obrigatório</div>
               </div>
               <div>
-                <input className="dsc-form-control" type="password" placeholder="Senha" />
+                <input onChange={handleInputChange} name='password' value={formData.password} className="dsc-form-control" type="password" placeholder="Senha" />
               </div>
             </div>
 
             <div className="dsc-login-form-buttons dsc-mt20">
-              <button type="submit" className="dsc-btn dsc-btn-blue">Entrar</button>
+              <button onClick={handleSubmit} type="submit" className="dsc-btn dsc-btn-blue">Entrar</button>
             </div>
           </form>
         </div>
