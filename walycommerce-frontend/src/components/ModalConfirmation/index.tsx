226a22f8fef5eec7cee687e40106b4a417db7f2b
walyson <walysonmoises1342@gmail.com>
2023-10-09 15:19:23 -0300

    modal confirmation

--- walycommerce-frontend/src/components/ModalConfirmation/index.tsx
+++ walycommerce-frontend/src/components/ModalConfirmation/index.tsx
@@ -14,13 +14,18 @@ const ModalConfirmation = ({message, onDialogClose, onConfirmationDialog}: prop)
         onDialogClose()
     }
 
+    const handleConfirmDialog = () => {
+        onConfirmationDialog(true)
+        onDialogClose()
+    }
+
     return (
         <div className="dsc-dialog-backgroud" onClick={() => onDialogClose()}>
             <div className='dsc-dialog-box' onClick={(e) => e.stopPropagation()}>
                 <h2>{message}</h2>
                 <div className='button-select-box'>
                 <button className='button-cancel-delete' onClick={handleClickCancelConfirmation}>Cancelar</button>
-                <div onClick={()=> onConfirmationDialog(true)} className='button-confirm-delete'><ButtonPrimary text='Confirmar'></ButtonPrimary></div>
+                <div onClick={handleConfirmDialog} className='button-confirm-delete'><ButtonPrimary text='Confirmar'></ButtonPrimary></div>
                 </div>
             </div>
         </div>
--- walycommerce-frontend/src/routes/Admin/ProductsList/index.tsx
+++ walycommerce-frontend/src/routes/Admin/ProductsList/index.tsx
@@ -6,104 +6,116 @@ import { findPageRequest } from '../../../services/productService'
 import { ProductDTO } from '../../../models/ProductModel'
 import SearchBar from '../../../components/SearchBar'
 import ButtonNextPage from '../../../components/ButtonNextPage'
-import ModalAlert from '../../../components/ModalAlert'
 import ModalConfirmation from '../../../components/ModalConfirmation'
+import ModalAlert from '../../../components/ModalAlert'
 
 type QueryParams = {
-    page: number;
-    name: string;
+  page: number;
+  name: string;
 }
 
 const ProductsList = () => {
 
-    const [queryParams, setQueryParams] = useState<QueryParams>({page: 0, name: ""})
-    const [products, setProducts] = useState<ProductDTO[]>()
-    const [isLastPage, setIsLastPage] = useState(false)
-    const [dialogInfoData, setDialogInfoData] = useState({visible: false, message: "Sucesso"})
-    const [dialogConfirmationData, setDialogConfirmationData] = useState({visible: false, confirm: false,message: "Sucesso"})
+  const [queryParams, setQueryParams] = useState<QueryParams>({ page: 0, name: "" })
+  const [products, setProducts] = useState<ProductDTO[]>()
+  const [isLastPage, setIsLastPage] = useState(false)
+  const [dialogInfoData, setDialogInfoData] = useState({ visible: false, message: "Sucesso" })
+  const [dialogConfirmationData, setDialogConfirmationData] = useState({ visible: false, message: "Sucesso" })
 
 
-    useEffect(()=>{
-        findPageRequest(queryParams.page, queryParams.name, 10)
-        .then(response => {
-            const nextPage = response.data.content
-            if(products){
-                setProducts(products.concat(nextPage))
-            }
-            else{
-                setProducts(nextPage)
-            }
-            setIsLastPage(response.data.last)
-            console.log(nextPage)
-        })
-    }, [queryParams])
+  useEffect(() => {
+    findPageRequest(queryParams.page, queryParams.name, 10)
+      .then(response => {
+        const nextPage = response.data.content
+        if (products) {
+          setProducts(products.concat(nextPage))
+        }
+        else {
+          setProducts(nextPage)
+        }
+        setIsLastPage(response.data.last)
+        console.log(nextPage)
+      })
+  }, [queryParams])
 
-    const handleSearch = (searchValue: string) => {
-        setProducts([])
-        setQueryParams({...queryParams, name: searchValue})
+  const handleSearch = (searchValue: string) => {
+    setProducts([])
+    setQueryParams({ ...queryParams, name: searchValue })
+  }
+
+  const onSetNextPage = () => {
+    setQueryParams({ ...queryParams, page: (queryParams.page + 1) })
+  }
+
+  const handleClickDeleteClose = () => {
+    setDialogConfirmationData({ ...dialogConfirmationData, visible: false })
+  }
+
+  const handleCLickCloseModalInfo = () => {
+    setDialogInfoData({ ...dialogInfoData, visible: false })
+  }
+
+  const handleDeleteClick = () => {
+    setDialogConfirmationData({ ...dialogConfirmationData, visible: true })
+
+  }
+
+  const handleConfirmationDelete = (confirm: boolean) => {
+    if (confirm) {
+      setDialogInfoData({ ...dialogInfoData, visible: true })
+      console.log("delete")
     }
-    
-    const onSetNextPage = () => {
-        setQueryParams({...queryParams, page: (queryParams.page + 1)})
-    }
-
-    const handleClickDeleteClose = () => {
-        setDialogInfoData({...dialogInfoData, visible: false})
-        setDialogConfirmationData({...dialogConfirmationData, visible: false})
-      }
-
-    const handleDeleteClick = () => {
-        setDialogInfoData({...dialogInfoData, visible: true})
-        setDialogConfirmationData({...dialogConfirmationData, visible: true})
-
-    }
-
+  }
 
   return (
     <main>
-    <section id="product-listing-section" className="dsc-container">
-      <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
+      <section id="product-listing-section" className="dsc-container">
+        <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
-      <div className="dsc-btn-page-container dsc-mb20">
-        <div className="dsc-btn dsc-btn-white">Novo</div>
-      </div>
+        <div className="dsc-btn-page-container dsc-mb20">
+          <div className="dsc-btn dsc-btn-white">Novo</div>
+        </div>
 
-      <SearchBar onsearchValue={(query) => handleSearch(query)}></SearchBar>
+        <SearchBar onsearchValue={(query) => handleSearch(query)}></SearchBar>
 
-      <table className="dsc-table dsc-mb20 dsc-mt20">
-        <thead>
-          <tr>
-            <th className="dsc-tb576">ID</th>
-            <th></th>
-            <th className="dsc-tb768">Preço</th>
-            <th className="dsc-txt-left">Nome</th>
-            <th></th>
-            <th></th>  
-          </tr>
-        </thead>
-        <tbody>
-          {products && 
-            products.map(item => (
-            <tr key={item.id}>
-            <td className="dsc-tb576">{item.id}</td>
-            <td><img className="dsc-product-listing-image" src={item.imgUrl} alt={item.name}/></td>
-            <td  className="dsc-tb768">R$ {item.price.toFixed(2)}</td>
-            <td  className="dsc-txt-left">{item.name}</td>
-            <td ><img className="dsc-product-listing-btn" src={editIcon} alt="Editar"/></td>
-            <td ><img onClick={handleDeleteClick} className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
+        <table className="dsc-table dsc-mb20 dsc-mt20">
+          <thead>
+            <tr>
+              <th className="dsc-tb576">ID</th>
+              <th></th>
+              <th className="dsc-tb768">Preço</th>
+              <th className="dsc-txt-left">Nome</th>
+              <th></th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {products &&
+              products.map(item => (
+                <tr key={item.id}>
+                  <td className="dsc-tb576">{item.id}</td>
+                  <td><img className="dsc-product-listing-image" src={item.imgUrl} alt={item.name} /></td>
+                  <td className="dsc-tb768">R$ {item.price.toFixed(2)}</td>
+                  <td className="dsc-txt-left">{item.name}</td>
+                  <td ><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
+                  <td ><img onClick={handleDeleteClick} className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
 
-      {!isLastPage && <div onClick={onSetNextPage}><ButtonNextPage></ButtonNextPage></div>}
-    </section>
-    {
+        {!isLastPage && <div onClick={onSetNextPage}><ButtonNextPage></ButtonNextPage></div>}
+      </section>
+      {
         dialogConfirmationData.visible &&
-        <ModalAlert message={dialogInfoData.message} onDialogClose={handleClickDeleteClose}></ModalAlert>
-        // <ModalConfirmation message='deseja realmente exculir esse poduto?' onDialogClose={handleClickDeleteClose}></ModalConfirmation>
-    }
-  </main>
+        <ModalConfirmation message='deseja realmente exculir esse poduto?' onDialogClose={handleClickDeleteClose} onConfirmationDialog={handleConfirmationDelete}></ModalConfirmation>
+      }
+      {
+        dialogInfoData.visible &&
+        <ModalAlert message={dialogInfoData.message} onDialogClose={handleCLickCloseModalInfo}></ModalAlert>
+
+      }
+    </main>
   )
 }
 
